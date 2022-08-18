d5d77fdac5ffffd23069ab21fea8d02eb37053d3
nilssvensson <nils27school@gmail.com>
2022-08-18 12:05:41 -0400

    commit message

--- script.js
+++ script.js
@@ -1,3 +1,44 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector(<script src="script.js"></script>).appendChild(h2);
+h2.textContent = "Have A Nice Day";
+document.querySelector("body").appendChild(h2);
+
+
+function addingEventListener() {
+  const input = document.getElementById('button');
+
+function clickAlert() {
+alert();
+}
+
+input.addEventListener('click', clickAlert);
+}
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
